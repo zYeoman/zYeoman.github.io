2b591d14bbd15690b746edd23bd56b78e163c319
Yongwen Zhuang <zyeoman@163.com>
2016-09-04 02:45:28 +0800

    :wrench: Add edit file tool
    新增ed.py工具, 用来更加方便快速的编辑文件.
    正常文件前面全TM是日期根本无法补全, 要重新编辑的话好累啊,
    于是就撸了这样的一个工具来快速编辑.

---
+++ ed.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# encoding:utf-8
+'''
+ed.py string
+Edit file whose name contain provide string.
+'''
+
+import os
+import sys
+import glob
+
+def main():
+    '''main function'''
+    if len(sys.argv) == 1:
+        exit()
+
+    files = glob.glob('*.md')
+
+    edit_files = list(filter(lambda x: sys.argv[1] in x, files))
+
+    if len(edit_files) == 1:
+        os.system('vim ' + edit_files[0])
+    else:
+        for i, filename in enumerate(edit_files):
+            print('{}: {}'.format(i, filename))
+        index = input('Please select a file[0]: ')
+        if index == '':
+            index = 0
+        else:
+            index = int(index)
+        os.system('vim ' + edit_files[index])
+
+# TODO 正则表达式支持
+# TODO argparse
+if __name__ == '__main__':
+    main()
