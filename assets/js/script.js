0c11beb30c87b3908c29e705458257aa2e2a078d
Yongwen Zhuang <zyeoman@163.com>
2017-12-05 16:35:48 +0800

    :ambulance: Fix link in post _blank

--- assets/js/script.js
+++ assets/js/script.js
@@ -103,7 +103,7 @@ $(document).on({
 // Re-run scripts for post content after pjax
 function afterPjax() {
   // Open links in new tab
-  $("#post__content").find("a").not("#markdown-toc a").attr({"rel":"noopener", "target":"_blank"});
+  $("#post__content").find("a").not("#markdown-toc a").not("[href^='#']").attr({"rel":"noopener", "target":"_blank"});
 
   $.getScript("//dn-lbstatics.qbox.me/busuanzi/2.3/busuanzi.pure.mini.js");
 
